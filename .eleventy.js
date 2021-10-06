344c35536b1b4e19d6e3b7d2a1383f11fbc602b0
Stefan Bajić <stefanbajic@posteo.net>
2021-10-06 14:48:32 +0200

    Prevent site crawling before we're done

--- .eleventy.js
+++ .eleventy.js
@@ -2,17 +2,18 @@ const svgContents = require('eleventy-plugin-svg-contents')
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('images')
+  eleventyConfig.addPassthroughCopy('robots.txt')
 
   eleventyConfig.addPlugin(svgContents)
 
   eleventyConfig.setBrowserSyncConfig({
-    files: './_site/css/**/*.css'
+    files: './_site/css/**/*.css',
   })
 
   return {
     dir: {
       input: 'src',
-      layouts: '_layouts'
-    }
+      layouts: '_layouts',
+    },
   }
 }
